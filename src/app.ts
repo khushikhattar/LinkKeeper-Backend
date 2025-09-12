6d61edf2aee7e47e7fa4153ebc8685e31be57592
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 00:11:38 +0530

    Resolving error

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,26 @@ import cors from "cors";
 import rootRouter from "./routes/index.routes";
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
-app.use((req, res, next) => {
-  console.log("Request:", req.method, req.path, "Origin:", req.headers.origin);
-  next();
-});
+const allowedOrigins = [
+  "https://link-keeper-frontend.vercel.app/",
+  "http://localhost:5173/", // for local dev (Vite default)
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin (like Postman, curl, server-to-server)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", rootRouter);
