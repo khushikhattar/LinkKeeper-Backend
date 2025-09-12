11bd77a520b1cc0374d2488d2ae1a7db7f757dc9
khushikhattar <khushikhattar06@gmail.com>
2025-09-12 18:53:43 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,20 @@ import rootRouter from "./routes/index.routes";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://link-keeper-frontend.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://link-keeper-frontend.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Authorization", "Content-Type"],
