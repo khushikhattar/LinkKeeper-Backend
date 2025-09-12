df993c368b4d95061167cc91ebb239fc9a2c10ba
khushikhattar <khushikhattar06@gmail.com>
2025-09-12 22:59:43 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,18 @@ import cors from "cors";
 import rootRouter from "./routes/index.routes";
 
 const app = express();
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      callback(null, origin);
-    },
-    credentials: true,
-  })
-);
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: Function) => {
+    callback(null, origin);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Authorization", "Content-Type"],
+};
+app.use(cors(corsOptions));
+app.options("/api/v1/*", cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
