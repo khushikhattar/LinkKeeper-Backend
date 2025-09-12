b63e39eeb0655aadabd8b9f1bb3e6f370527ec82
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 00:03:15 +0530

    Resolving error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import rootRouter from "./routes/index.routes";
 
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
+app.use((req, res, next) => {
+  console.log("Request:", req.method, req.path, "Origin:", req.headers.origin);
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", rootRouter);
