79ff1877c7f79d33d5b899c9319b248fe53e22ef
khushikhattar <khushikhattar06@gmail.com>
2025-09-12 18:56:35 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import rootRouter from "./routes/index.routes";
+
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://link-keeper-frontend.vercel.app",
 ];
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -25,5 +27,8 @@ app.use(
   })
 );
 
+app.options("*", cors());
+
 app.use("/api/v1", rootRouter);
+
 export { app };
