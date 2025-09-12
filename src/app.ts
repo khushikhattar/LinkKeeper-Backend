76a03fe0af3ba76ee66d942e83a6583a97e9cd10
khushikhattar <khushikhattar06@gmail.com>
2025-09-12 20:14:24 +0530

    Resolving Cors Issue

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,27 @@ import cors from "cors";
 import rootRouter from "./routes/index.routes";
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://link-keeper-frontend.vercel.app",
+];
 
-const allowedOrigins = ["https://link-keeper-frontend.vercel.app"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-  })
-);
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Authorization", "Content-Type"],
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204);
-});
 
 app.use("/api/v1", rootRouter);
 
