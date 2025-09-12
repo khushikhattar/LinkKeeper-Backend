595ea0f056f4d5d3c0e9e513fe6e74bfd8b23b97
khushikhattar <khushikhattar06@gmail.com>
2025-09-12 21:31:58 +0530

    Resolving Cors Issue

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,21 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Origin"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
