1a348f937c7a91f466f4706cdc6a4f2e69b3ee49
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 09:55:47 +0530

    Resolving Errors

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,21 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import rootRouter from "./routes/index.routes";
 
 const app = express();
-const allowedOrigins = [
-  "https://link-keeper-frontend.vercel.app/",
-  "http://localhost:5173/", // for local dev (Vite default)
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // allow requests with no origin (like Postman, curl, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://link-keeper-frontend.vercel.app",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", rootRouter);
