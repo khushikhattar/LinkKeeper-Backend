ec148a6c9b63f32a248010873541e40459744731
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 13:40:44 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://link-keeper-coral.vercel.app",
+  "https://link-keeper-frontend.vercel.app",
   // Add any other production domains here
 ];
 
