2486508d7bbfe7c822bcfd42cf661d4fa07d02b6
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 17:06:33 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,42 @@
-import express from "express";
-import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import rootRouter from "./routes/index.routes";
 
 const app = express();
-const allowedOrigins = [
-  "https://link-keeper-frontend.vercel.app",
-  "http://localhost:5173",
-];
-const previewRegex = /\.vercel\.app$/;
-const corsOptions: cors.CorsOptions = {
-  origin: (origin, cb) => {
-    if (!origin) return cb(null, true);
-    if (allowedOrigins.includes(origin) || previewRegex.test(origin)) {
-      return cb(null, true);
-    }
-    return cb(new Error("Not allowed by CORS"));
-  },
-  credentials: true,
-  methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  exposedHeaders: ["Content-Length"],
-  optionsSuccessStatus: 204,
-};
 
-app.use(cors(corsOptions));
-app.options("/{*path}", cors(corsOptions));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://link-keeper-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "Cookie",
+      "Set-Cookie",
+    ],
+    exposedHeaders: ["Set-Cookie"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use("/api/v1", rootRouter);
 export { app };
