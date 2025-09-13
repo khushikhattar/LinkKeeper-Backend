a37c50241be163a14950aff0d05f2fbcb1d74dcb
khushikhattar <khushikhattar06@gmail.com>
2025-09-13 10:40:50 +0530

    Modified Files

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,8 @@
 import userRouter from "./user.routes";
 import tagRouter from "./tag.routes";
 import contentRouter from "./content.routes";
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 const router = Router();
-router.get("/health", (req: Request, res: Response) => {
-  res.send({ message: "Server is healthy" });
-});
 router.use("/users", userRouter);
 router.use("/tags", tagRouter);
 router.use("/content", contentRouter);
