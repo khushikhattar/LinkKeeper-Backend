dd6a7191e8cf32dfd552dcbad30373818eb0efcb
khushikhattar <khushikhattar06@gmail.com>
2025-09-17 16:01:49 +0530

    Modified Files

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,7 @@ import rootRouter from "./routes/index.routes";
 import { loggingMiddleware } from "./middlewares/logging.middleware";
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://link-keeper-frontend.vercel.app",
-];
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-    ],
-    exposedHeaders: ["Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(loggingMiddleware);
