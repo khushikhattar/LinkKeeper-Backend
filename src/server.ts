a9008b0c2b6a95de215186e5c7bc00f87bbff3f3
Khushi Khattar <151365990+khushikhattar@users.noreply.github.com>
2025-09-17 13:24:57 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import app from "./app";
+import { app } from "./app";
+import mongoose from "mongoose";
+import serverless from "serverless-http";
 
 dotenv.config();
 
-const databaseURL = process.env.MONGODB_URI;
-
-if (!databaseURL) {
-  throw new Error("Database URL is not defined in environment variables");
-}
-
-mongoose
-  .connect(databaseURL)
-  .then(() => console.log("✅ Connected to MongoDB successfully"))
-  .catch((err) => console.error("❌ Failed to connect to MongoDB", err));
-
-export default app;
+const startServer = async () => {
+  const databaseURL = process.env.MONGODB_URI;
+  if (!databaseURL) {
+    console.error("Database URL is not defined in environment variables");
+    process.exit(1);
+  }
+  try {
+    await mongoose.connect(databaseURL);
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+};
+startServer();
+export const handler = serverless(app);
